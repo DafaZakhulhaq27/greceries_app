fa80ba3177f921ec8e14d46e561065918e6d05b6
Dafa Zakhulhaq <dafa27890@gmail.com>
2022-06-18 16:38:06 +0700

    initial commit

---
+++ android/app/src/main/kotlin/com/dafazakhulhaq/groceries_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.dafazakhulhaq.groceries_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/theme.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color white = Colors.white;
+
+Color lightFontDark = const Color(0xff06161C);
+Color lightFontGrey = const Color(0xff979899);
+Color lightFontWhite = const Color(0xffFFFFFF);
+Color lightFontPrimary = const Color(0xff23AA49);
+Color lightFontSecondary = const Color(0xffFF324B);
+Color lightFontStroke = const Color(0xffF1F1F5);
+Color lightFontLightGrey = const Color(0xffE0E0E0);
+Color lightFontLightGrey2 = const Color(0xffF3F5F7);
+
+Color darkFontGrey = const Color(0xff617986);
+Color darkFontDarkBG = const Color(0xff0D1F29);
+Color darkFontDarkBG2 = const Color(0xff1A3848);
+Color darkColorDarkBG3 = const Color(0xff2D5368);
+Color darkColorStroke = const Color(0xff203744);
+
+Color fuschia100 = const Color(0xffEF5DA8);
+Color fuschia80 = const Color(0xffF178B6);
+Color fuschia60 = const Color(0xffFCDDEC);
+Color iris100 = const Color(0xff5D5FEF);
+Color iris80 = const Color(0xff7879F1);
+Color iris60 = const Color(0xffA5A6F6);
+
+TextStyle heading28Bold = GoogleFonts.dmSans(
+  fontWeight: FontWeight.w700,
+  fontSize: 28,
+);
+TextStyle heading24Bold = GoogleFonts.dmSans(
+  fontWeight: FontWeight.w700,
+  fontSize: 24,
+);
+TextStyle heading20Bold = GoogleFonts.dmSans(
+  fontWeight: FontWeight.w700,
+  fontSize: 20,
+);
+TextStyle heading18Bold = GoogleFonts.dmSans(
+  fontWeight: FontWeight.w700,
+  fontSize: 18,
+);
+TextStyle body16Medium = GoogleFonts.dmSans(
+  fontWeight: FontWeight.w500,
+  fontSize: 16,
+);
+TextStyle body14Medium = GoogleFonts.dmSans(
+  fontWeight: FontWeight.w500,
+  fontSize: 14,
+);
+TextStyle body16Bold = GoogleFonts.dmSans(
+  fontWeight: FontWeight.w700,
+  fontSize: 16,
+);
+TextStyle body12Medium = GoogleFonts.dmSans(
+  fontWeight: FontWeight.w500,
+  fontSize: 12,
+);
